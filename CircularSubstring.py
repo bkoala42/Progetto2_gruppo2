2a4c181c46666a3bf2ac4dae9fbbe65a110b5170
JBFourierous <marco.carpentiero@gmail.com>
2018-11-13 19:58:24 +0100

    Added exercise 4

---
+++ CircularSubstring.py
@@ -0,0 +1,12 @@
+from TdP_collections.text.find_kmp import find_kmp
+
+
+def circular_substring(P, T):
+    m, n = len(P), len(T)
+#    new_str = T[m-1:n] + T[0:m-1]
+    new_str = T[n-(m+2):n] + T[0:2]
+    print(new_str)
+    if find_kmp(new_str, P) != -1:
+        return True
+    return False
+
