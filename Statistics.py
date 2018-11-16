0fd60f949f5cb666d85423bd54e0df22d2c96000
JBFourierous <marco.carpentiero@gmail.com>
2018-11-16 20:22:53 +0100

    Updated Statistics

--- Statistics.py
+++ Statistics.py
@@ -15,10 +15,7 @@ class Statistics:
                 except FileNotFoundError:
                     print("File not found")
                 else:
-                    while 1:
-                        line = file.readline()
-                        if not line:
-                            break
+                    for line in file:
                         tmp = line.split(":")
                         key = tmp[0]
                         value = tmp[1]
@@ -32,16 +29,24 @@ class Statistics:
                 total = value[1] + v
                 tmp.append(frequency)
                 tmp.append(total)
-                self.avl[k] = tmp
             else:
                 tmp.append(1)
                 tmp.append(v)
-                self.avl[k] = tmp
+            self.avl[k] = tmp
 
         def len(self):
+            """
+            Returns the number of keys in the map in O(n) time, where n is the number of keys.
+            :return: Number of keys in the map
+            """
             return len(self.avl.keys())
 
         def occurrences(self):
+            """
+            Calculates the sum of the frequencies of the elements in the map by iteration in time O(n), where n is
+            the number of the keys.
+            :return: Sum of the frequences of the elements in the map.
+            """
             value = self.avl.values()
             total = 0
             for i in value:
@@ -49,6 +54,11 @@ class Statistics:
             return total
 
         def average(self):
+            """
+            Calculates the mean value of the values of the elements in the map by iteration in time O(n), where n is
+            the number of the keys.
+            :return: Mean value of the values of the elements in the map.
+            """
             value = self.avl.values()
             total = 0
             for i in value:
@@ -56,26 +66,35 @@ class Statistics:
             return total/self.len()
 
         def median(self):
+            """
+            NON SONO SICURO DELLA COMPLESSITA', VA RICALCOLATA CON ATTENZIONE
+            Calculates the central key of the set of keys taking into account their frequency in time O(k) where k is
+            the number of the occurrences in the dataset.
+            :return: Median of the keys in the map.
+            """
             if self.len() == 0:
+                # if the map is empty no median available
                 return None, None
             else:
                 tmp = []
                 for node in self.avl:
+                    #for each node fill an array with the occurrences
                     frequency = self.avl.get(node)[0]
                     if len(tmp) == 0:
                         tmp = [node] * frequency
                     else:
                         tmp1 = [node] * frequency
                         tmp.extend(tmp1)
-                lenght = len(tmp)
-                if lenght % 2 == 1:
-                    return tmp[int(lenght/2)],None
+                if len(tmp) % 2 == 1:
+                    # if even number of occurrences return the exact central values
+                    return tmp[int(len(tmp)/2)],None
                 else:
-                    return tmp[int(lenght/2)], tmp[int(lenght/2 + 1)]
+                    # if odd number of occurences return the two values in the middle of the array
+                    return tmp[int(len(tmp)/2)], tmp[int(len(tmp)/2 + 1)]
 
-        def percentile(self, j):
+        def percentile(self, j = 20):
             tmp = []
-            if j <0 or j > 100:
+            if j > 100:
                 raise Exception("j must be between [0:99]")
             else:
                 for node in self.avl:
