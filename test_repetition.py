74c0bc78b8fc35451470eaadd4d3ddb3ba31bf15
bkoala42 <ilariagigi@virgilio.it>
2018-11-16 20:19:55 +0100

    armandello

--- test_repetition.py
+++ test_repetition.py
@@ -3,21 +3,23 @@ from repetitions import find_repetition
 """ In order to test this utility it is necessary to create two directories with ONLY text files. One with duplicated
     files with both equal names and different names, and one without duplicated files."""
 
+
 def test_find_repetition_collide():
     """ Tests find_repetition in case of directory with duplicated files. """
     dir_test = "Colliding"
     collides = find_repetition(dir_test)
-    #add to this condition the name of the duplicate files
+    # add to this condition the name of the duplicate files
     if len(collides) != 0 and 'File2.txt' in collides and 'File3.txt' in collides and 'File5.txt' in collides:
         print("Test test_find_repetition_collide passed")
     else:
         print("Test test_find_repetition_collide failed")
 
+
 def test_find_repetition_multiple_collide():
     """ Tests find_repetition in case of directory with more than one duplicated files. """
     dir_test = "MultipleColliding"
     collides = find_repetition(dir_test)
-    #add to this condition the name of the duplicate files
+    # add to this condition the name of the duplicate files
     if len(collides) != 0 and 'File2.txt' in collides and 'File3.txt' in collides and 'File5.txt' in collides \
             and 'File1.txt' in collides and 'File6.txt' in collides:
         print("Test test_find_repetition_multiple_collide passed")
@@ -35,6 +37,11 @@ def test_find_repetition_no_collide():
         print("Test test_find_repetition_no_collide failed")
 
 
-test_find_repetition_collide()
-test_find_repetition_no_collide()
-test_find_repetition_multiple_collide()
+def run_test_repetition():
+    test_find_repetition_collide()
+    test_find_repetition_no_collide()
+    test_find_repetition_multiple_collide()
+
+
+if __name__ == "__main__":
+    run_test_repetition()
