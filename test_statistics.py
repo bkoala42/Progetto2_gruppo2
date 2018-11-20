d0d37f9ad1de4ba637a4b82cc85e51eef635857d
Army96 <43437259+Army96@users.noreply.github.com>
2018-11-20 20:25:48 +0100

    Update test_statistics.py

--- test_statistics.py
+++ test_statistics.py
@@ -1,13 +1,12 @@
 from Statistics import Statistics
 
-
 def test_add():
-    stat1 = Statistics("empity.txt")
+    stat1 = Statistics("empty.txt")
     stat1.add("Ampere", 1)
     stat1.add("Tesla", 1)
     stat1.add("Edison", 2)
 
-    stat2 = Statistics("empity.txt")
+    stat2 = Statistics("empty.txt")
     stat2.add("Plank", 3)
     stat2.add("Newton", 5)
     stat2.add("Schrodinger", 8)
@@ -20,7 +19,7 @@ def test_add():
 
 
 def test_len():
-    stat = Statistics("empity.txt")
+    stat = Statistics("empty.txt")
     if stat.len() == 0:
         stat.add("Plank", 1)
         stat.add("Schrodinger", 1)
@@ -33,7 +32,7 @@ def test_len():
 
 
 def test_occurrences():
-    stat = Statistics("empity.txt")
+    stat = Statistics("empty.txt")
     if stat.occurrences() == 0:
         stat.add("Plank", 1)
         stat.add("Newton", 1)
@@ -50,7 +49,7 @@ def test_occurrences():
 
 
 def test_average():
-    stat = Statistics("empity.txt")
+    stat = Statistics("empty.txt")
     if stat.occurrences() == 0:
         stat.add("Plank", 1)
         stat.add("Newton", 1)
@@ -58,7 +57,7 @@ def test_average():
         stat.add("Kepler", 3)
         stat.add("Plank", 5)
         stat.add("Schrodinger", 8)
-        if stat.average() == (1 + 1 + 2 + 3 + 5 + 8) / stat.len():
+        if stat.average() == (1 + 1 + 2 + 3 + 5 + 8) / stat.occurrences():
             print("Test test_average passed")
         else:
             print("Test test_average failed")
@@ -67,49 +66,35 @@ def test_average():
 
 
 def test_median():
-    stat1 = Statistics("empity.txt")
-    stat2 = Statistics("empity.txt")
-    r1, r2 = stat1.median()
-    r3, r4 = stat2.median()
-    if r1 == r2 == r3 == r4 is None:
+    stat = Statistics("empty.txt")
+    r1 = stat.median()
+    if r1 is None:
         for i in range(9):
-            stat1.add(chr(65 + i), i + 1)
-            stat2.add(chr(74 + i), 10 + i)
-        stat2.add(chr(74 + 8 + 1), 19)
-        if stat1.len() % 2 == 1 and stat2.len() % 2 == 0:
-            r1, r2 = stat1.median()
-            r3, r4 = stat2.median()
-            if r1 == "E" and r2 is None and r3 == "O" and r4 == "P":
-                print("Test test_median passed")
-            else:
-                print("Test test_median failed")
+            stat.add(chr(65 + i), i + 1)
+        r1 = stat.median()
+        if r1 == "E":
+            print("Test test_median passed")
         else:
             print("Test test_median failed")
     else:
         print("Test test_median failed")
 
-#nel test genero un vettore che contiene l'alfabeto da A a J.
-# il 25% percentile dovrebbe essere C ma l'algoritmo restituisce J in quanto mi genera in maniera errata il vettore su cui calcolare il percententile
-#in particolare esce questo ['A', 'J', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B']
-#ho lasciato una stampa nel codice di percentile() nella classe Statistic
-def test_percentile_file_empity():
-    stat = Statistics("empity.txt")
+def test_percentile_file_empty():
+    stat = Statistics("empty.txt")
     if stat.len() == 0:
         for i in range(10):
             stat.add(chr(65 + i), i + 1)
         if stat.percentile(20) == "B":
-            print("Test test_percentile_file_empity passed")
+            print("Test test_percentile_file_empty passed")
         else:
-            print("Test test_percentile_file_empity failed")
+            print("Test test_percentile_file_empty failed")
     else:
-        print("Test test_percentile_file_empity failed")
+        print("Test test_percentile_file_empty failed")
 
-#In questo test il 25% percentile è Mila e quindi il risultato è giusto ma il vettore utilizzato per calcolarlo non rispetta l'ordine imposto
-#ho lasciato una stampa nel codice di percentile() nella classe Statistic
 def test_percentile_file_dataset():
     stat = Statistics("dataset.txt")
     if stat.len() != 0:
-        if stat.percentile(25) == "Mila":
+        if stat.percentile(25) == "Kaori":
             print("Test test_percentile_file_dataset passed")
         else:
             print("Test test_percentile_file_dataset failed")
@@ -117,7 +102,7 @@ def test_percentile_file_dataset():
         print("Test test_percentile_file_dataset failed")
 
 def test_mostFrequent():
-    stat = Statistics("empity.txt")
+    stat = Statistics("empty.txt")
     if stat.len() == 0 and stat.mostFrequent(2) is None:
         stat.add("Plank", 1)
         stat.add("Newton", 1)
@@ -140,9 +125,9 @@ def run_test_statistics():
     #test_occurrences()
     #test_average()
     #test_median()
-    test_percentile_file_dataset()
-    test_percentile_file_empity()
-    #test_mostFrequent()
+    #test_percentile_file_dataset()
+    #test_percentile_file_empty()
+    test_mostFrequent()
 
 
 if __name__ == "__main__":
